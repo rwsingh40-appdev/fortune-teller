c35849dce07a02e66d866d8437a26c4a8a11c9e2
Raj Singh <rsingh40@chicagobooth.edu>
2023-04-13 14:14:19 +0000

    1

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -7,7 +7,20 @@ class NumbersController < ApplicationController
       
       @zebra.push(giraffe)
     end
-
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+
+  def losers
+    @zebra = Array.new
+    
+    5.times do
+      giraffe = rand(1...100)
+      
+      @zebra.push(giraffe)
+    end
+
+    render({:template => "lottery_stuff/boo.html.erb"})
+  end
+
 end
